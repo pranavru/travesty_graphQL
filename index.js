de9edbe0c033665ae93d4091d392f25d3815018d
Pranav Ruparelia <pranavruparelia9@gmail.com>
2020-03-13 18:47:58 -0400

    Created Todo Schema  and connected to MongoDb

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
-const schema = require('./model/schema');
-
+const schema = require('./routers/schema');
+const mongoose = require('mongoose');
+const config = require('config');
 const app = express();
+var graphql = require ('graphql').graphql  
+const schema_todos = require('./routers/schema_todos')
 
-app.use('/graphql', expressGraphQL({
-    schema : schema,
-    graphiql: true
-}))
+
+const mongooseConnection = mongoose.connection;
+mongoose.connect(config.get("dbConnectionString"));
+mongooseConnection.on("connected", () => {
+    console.log("Server has established a mongoose connection")
+});
+
+// var query = 'mutation {  add(title: "Read a book") { id, title } }';
+// graphql(schema_todos, query).then(function (result) {
+//     console.log(JSON.stringify(result, null, " "));
+// });
+
+// app
+//   .use('/', expressGraphQL({ schema: schema_todos, pretty: true }))
+//   .listen(4000, function (err) {
+//     console.log('GraphQL Server is now running on localhost:4000');
+//   });
+
+app.get('/', () => {
+    console.log("$$$$$ ----- Connected ----- $$$$$\n Welcome to GrossTracking Application")
+})
 
 app.listen(4000, () => {
     console.log("The server is running on port 4000");
 })
+
---
+++ routers/schema_todos.js
@@ -0,0 +1,84 @@
+const graphql = require('graphql');
+const TODOs = [
+    {
+        "id": 1446412739542,
+        "title": "Read emails",
+        "completed": false
+    },
+    {
+        "id": 1446412740883,
+        "title": "Buy orange",
+        "completed": true
+    }
+];
+
+const TodoType = new graphql.GraphQLObjectType({
+    name: 'todo',
+    fields: function () {
+        return {
+            id: {
+                type: graphql.GraphQLID
+            },
+            title: {
+                type: graphql.GraphQLString
+            },
+            completed: {
+                type: graphql.GraphQLBoolean
+            }
+        }
+    }
+});
+
+const queryType = new graphql.GraphQLObjectType({
+    name: 'Query',
+    fields: function () {
+        return {
+            todos: {
+                type: new graphql.GraphQLList(TodoType),
+                resolve: function () {
+                    return new Promise(function (resolve, reject) {
+                        setTimeout(function () {
+                            resolve(TODOs)
+                        }, 4000)
+                    });
+                }
+            }
+        }
+    }
+});
+
+var MutationAdd = {
+    type: TodoType,
+    description: 'Add a Todo',
+    args: {
+        title: {
+            name: 'Todo title',
+            type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+        }
+    },
+    resolve: (root, args) => {
+        var newTodo = new TODO({
+            title: args.title,
+            completed: false
+        })
+        newTodo.id = newTodo._id
+        return new Promise((resolve, reject) => {
+            newTodo.save(function (err) {
+                if (err) reject(err)
+                else resolve(newTodo)
+            })
+        })
+    }
+}
+
+var MutationType = new graphql.GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        add: MutationAdd
+    }
+});
+
+module.exports = new graphql.GraphQLSchema({
+    query: queryType,
+    mutation: MutationType
+});
